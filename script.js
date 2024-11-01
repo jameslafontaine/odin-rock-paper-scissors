0cd252a56122fcea60c6a888f15beb210e3454c0
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-01 22:28:18 +1100

    Implement the logic to play a single round

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function getComputerChoice() {
 // Write the logic to get the human choice
 
 function getHumanChoice() {
-    humanChoice = prompt("Enter your choice (rock, paper, scissors): ");
+    humanChoice = prompt("Enter your choice (rock, paper, scissors): ").toLowerCase();
 
     if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
         return humanChoice
@@ -24,3 +24,46 @@ function getHumanChoice() {
         return "rock"
     }
 }
+
+// Declare the players score variables
+
+humanScore = 0
+computerScore = 0
+
+// Write the logic to play a single round
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log("Draw!")
+        } else if (computerChoice == "paper") {
+            console.log("You lose! Paper beats Rock.")
+        } else {
+            console.log("You win! Rock beats Scissors.")
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("You win! Paper beats Rock.")
+        } else if (computerChoice == "paper") {
+            console.log("Draw!")
+        } else {
+            console.log("You lose! Scissors beats Paper.")
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("You lose! Rock beats Scissors.")
+        } else if (computerChoice == "paper") {
+            console.log("You win! Scissors beats Paper.")
+        } else {
+            console.log("Draw!")
+        }
+    }
+    console.log()
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
