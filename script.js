ff0499b303f28407925b0519325a0a5570282aaa
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-01 22:18:38 +1100

    Implement the logic to get the human choice

--- script.js
+++ script.js
@@ -3,13 +3,24 @@
 function getComputerChoice() {
     randNum = Math.random() * 3;
 
-    // console.log(randNum);
-
     if (randNum < 1) {
         return "rock";
     } else if (randNum < 2) {
         return "paper"
     } else {
         return "scissors"
+    }
+}
+
+// Write the logic to get the human choice
+
+function getHumanChoice() {
+    humanChoice = prompt("Enter your choice (rock, paper, scissors): ");
+
+    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors") {
+        return humanChoice
+    } else {
+        // Return rock as a backup for invalid input
+        return "rock"
     }
 }
